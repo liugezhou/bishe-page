91865d53b176667a3c92c43ed62186d7f066a1b0
liugezhou <18231133236@163.com>
2024-11-01 16:59:27 +0800

    first commit

---
+++ .vuepress/config.ts
@@ -0,0 +1,127 @@
+import { defineUserConfig } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+import { viteBundler } from '@vuepress/bundler-vite'
+import { webpackBundler } from '@vuepress/bundler-webpack'
+
+export default defineUserConfig({
+  title: "优秀毕设项目",
+  description: "毕设交流",
+  bundler: viteBundler(),
+  // bundler: webpackBundler(),
+  theme: recoTheme({
+    style: "@vuepress-reco/style-default",
+    logo: "/logo.png",
+    author: "liugezhou",
+    authorAvatar: "/head.png",
+    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {
+      "/docs/theme-reco/": [
+        {
+          text: "module one",
+          children: ["home", "theme"],
+        },
+        {
+          text: "module two",
+          children: ["api", "plugin"],
+        },
+      ],
+    },
+    navbar: [
+      { text: "首页", link: "/" },
+      { text: "案例", link: "/categories/reco/1.html" },
+      { text: "标签", link: "/tags/tag1/1.html" },
+      {
+        text: "文档",
+        children: [
+          { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
+          { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
+        ],
+      },
+    ],
+    // 公告栏
+    bulletin: {
+      body: [
+        {
+          type: "text",
+          content: `🎉🎉🎉 优秀毕设项目更新了，大家可以尽情尝鲜了，并且希望大家在 QQ 群 踊跃反馈使用体验，我会在第一时间响应。`,
+          style: "font-size: 12px;",
+        },
+        {
+          type: "hr",
+        },
+        {
+          type: "title",
+          content: "QQ 群",
+        },
+        {
+          type: "text",
+          content: `
+          <ul>
+            <li>QQ群：392177763</li>
+          </ul>`,
+          style: "font-size: 12px;",
+        },
+         {
+          type: "hr",
+        },
+        {
+          type: "title",
+          content: "微信咨询",
+        },
+        {
+          type: "text",
+          content: `
+          <image src="https://jsd.cdn.zzko.cn/gh/liugezhou/picx-images-hosting@master/bishe/zhangtianzhi.webp"></image>`
+          ,
+          style: "font-size: 12px;",
+        },
+        // {
+        //   type: "hr",
+        // },
+        // {
+        //   type: "title",
+        //   content: "GitHub",
+        // },
+        // {
+        //   type: "text",
+        //   content: `
+        //   <ul>
+        //     <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+        //     <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+        //   </ul>`,
+        //   style: "font-size: 12px;",
+        // },
+        {
+          type: "hr",
+        },
+        {
+          type: "buttongroup",
+          children: [
+            {
+              text: "打赏",
+              link: "/docs/others/donate.html",
+            },
+          ],
+        },
+      ],
+    },
+    // commentConfig: {
+    //   type: 'valine',
+    //   // options 与 1.x 的 valineConfig 配置一致
+    //   options: {
+    //     // appId: 'xxx',
+    //     // appKey: 'xxx',
+    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
+    //     // verify: true, // 验证码服务
+    //     // notify: true,
+    //     // recordIP: true,
+    //     // hideComments: true // 隐藏评论
+    //   },
+    // },
+  }),
+  // debug: true,
+});
