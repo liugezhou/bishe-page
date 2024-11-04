da6ee2faafefcbdcfedbd4b849c25d476bc530db
liugezhou <18231133236@163.com>
2024-11-04 12:58:22 +0800

    feat:国际化文本

---
+++ .vuepress/components/Carousel.vue
@@ -0,0 +1,31 @@
+<template>
+  <carousel :autoplay="true" :loop="true">
+    <slide v-for="(image, index) in images" :key="index">
+      <img :src="image" alt="" />
+    </slide>
+  </carousel>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+
+export default {
+  components: {
+    Carousel,
+    Slide
+  },
+  props: {
+    images: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+img {
+  width: 100%;
+  height: auto;
+}
+</style>
