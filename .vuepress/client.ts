47faa3412b230481e042f1d555ba9ac7d98c9472
liugezhou <18231133236@163.com>
2024-11-05 12:34:51 +0800

    fix:百度统计代码优化

--- .vuepress/client.ts
+++ .vuepress/client.ts
@@ -2,18 +2,20 @@ import { defineClientConfig } from 'vuepress/client'
 import Swiper from './components/Swiper.vue'
 
 export default defineClientConfig({
-  enhance({ app,router }) {
+  enhance({ app, router }) {
     app.component('Swiper', Swiper)
-    // 百度统计代码
-    const hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?b102e17dcedf0ede8628680519a5229b";
-    const s = document.getElementsByTagName("script")[0];
-    s && s.parentNode && s.parentNode.insertBefore(hm, s);
-    // 监听路由变化
-    router.afterEach((to, from) => {
-      if (window._hmt) {
-        window._hmt.push(['_trackPageview', to.fullPath]);
-      }
-    });
-  }
+    if (typeof window !== 'undefined') {
+      // 百度统计代码
+      const hm = document.createElement('script')
+      hm.src = 'https://hm.baidu.com/hm.js?b102e17dcedf0ede8628680519a5229b'
+      const s = document.getElementsByTagName('script')[0]
+      s && s.parentNode && s.parentNode.insertBefore(hm, s)
+      // 监听路由变化
+      router.afterEach((to, from) => {
+        if (window._hmt) {
+          window._hmt.push(['_trackPageview', to.fullPath])
+        }
+      })
+    }
+  },
 })
